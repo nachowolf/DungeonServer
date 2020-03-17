175942198406b791044e0a25f6029bdcd6f793e8
nachowolf <nathrijacobs@gmail.com>
2020-03-17 08:14:32 +0200

    added encrypt api

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,19 @@
 import api.Api;
-import database.CharacterDatabase;
+
 
 import static spark.Spark.*;
 
 
 public class Main {
-        public static void main(String[] args) {
 
-          Api api = new Api(new CharacterDatabase());
-          get("/api/characters/add/:name", api.addCharacter());
-            get("/api/characters", api.getCharacters());
+    public static void main(String[] args) {
+        Api api = new Api();
+        post("/atbash/encrypt/", api.encryptAtbash());
+        get("/atbash/encrypted/", api.encryptedAtbash());
 
-        }
+        get("/", (req, res) -> "Hello Wolrd");
+
+        post("/test", api.test());
+    }
+
 }
--- src/main/java/api/Api.java
+++ src/main/java/api/Api.java
@@ -1,31 +1,65 @@
 package api;
 
+import ciphers.Atbash;
 import com.google.gson.Gson;
 
+import com.google.gson.JsonElement;
 import dungeon.Character;
 import spark.Route;
 
 public class Api {
 
+    private JsonElement gson;
 
-    public Route getCharacters(){
+
+    public Route encryptAtbash() {
         return (req, res) -> {
             res.type("application/json");
+            BodyExtractor extract = new Gson().fromJson(req.body(), BodyExtractor.class);
+            return new Gson().toJsonTree(new JsonResponse(Status.SUCCESSS, new Atbash().encrypt(extract.getData())
+
+            ));
+
+        };
+
+    }
+
+        public Route encryptedAtbash(){
+            return (req, res) -> {
+                res.type("application/json");
+                return  gson;
+
+            };
+    }
+
+    public Route test(){
+        return (req, res) -> {
+            res.type("application/json");
+            BodyExtractor extract = new Gson().fromJson(req.body(), BodyExtractor.class);
+            System.out.println(extract.getData());
             return new Gson().toJson(new JsonResponse(Status.SUCCESSS));
+
         };
     }
 
-    public Route addCharacter(){
-        return (req, res) -> {
-             return new Gson().toJson(new JsonResponse(Status.SUCCESSS));
-//            return "Added";
-        };
-    }
+//    public Route getCharacters(){
+//        return (req, res) -> {
+//            res.type("application/json");
+//            return new Gson().toJson(new JsonResponse(Status.SUCCESSS));
+//        };
+//    }
+//
+//    public Route addCharacter(){
+//        return (req, res) -> {
+//             return new Gson().toJson(new JsonResponse(Status.SUCCESSS));
+////            return "Added";
+//        };
+//    }
 
-    public Route updateCharacter(){
-        return (req, res) -> {
-            return new Gson().toJson(new JsonResponse(Status.SUCCESSS));
-//            return "Added";
-        };
-    }
+//    public Route updateCharacter(){
+//        return (req, res) -> {
+//            return new Gson().toJson(new JsonResponse(Status.SUCCESSS));
+////            return "Added";
+//        };
+//    }
 }
